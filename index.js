7e5e6a1fc1998fc50d32cda88abe8571218af00e
Mustafa Abdo <mustafa.abdo4941@gmail.com>
2024-02-15 18:18:31 +0200

    UpdateByMG/15Feb

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cartRouter = require("./routes/cartRoute");
+const ejs = require("ejs");
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+const port = process.env.PORT || 3000;
 app.use("/api/v1/cart", cartRouter);
+app.set("view engine", "ejs");
+
+// ------------------------------------------------------------------------------------------- Main Root
+app.get("/", (req, res) => {
+  res.render("profile.ejs");
+});
+// ------------------------------------------------------------------------------------------- Main Root
 mongoose
-  .connect("mongodb://127.0.0.1:27017/ECommerce")
+  .connect("mongodb://localhost:27017/ECommerce")
   .then(() => {
-    console.log("Connected To Database");
+    console.log("Connected To Database Succssefuly");
   })
   .catch((Error) => {
     console.log(`Error In Connection: ${Error}`);
   });
 
-app.listen(3000);
-// changing from 
-//commint
-//commint2
-//another test
+app.listen(port, () => {
+  console.log(`Connected To Port ${port}`);
+});
