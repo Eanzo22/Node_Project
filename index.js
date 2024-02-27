17f7d11acf25a56a86a3d25d5020de8b37480d3e
Mustafa Abdo <mustafa.abdo4941@gmail.com>
2024-02-27 17:45:13 +0200

    Payment Handling

--- index.js
+++ index.js
@@ -6,10 +6,11 @@ const categoriesRouter = require("./routes/categoryRoute");
 const productRouter = require("./routes/productRoute");
 const ordersRouter = require("./routes/orderRouter");
 const userRouter = require("./routes/user.router");
-const adminRouter=require('./routes/admin.router');
+const adminRouter = require("./routes/admin.router");
+const paymentRouter = require("./routes/payment.router");
 const app = express();
-const {userAuth} = require('./middleware/userAuth')
-const {adminAuth} = require('./middleware/adminAuth')
+const { userAuth } = require("./middleware/userAuth");
+const { adminAuth } = require("./middleware/adminAuth");
 require("./DB/index");
 
 //////////////////////////////////
@@ -19,7 +20,7 @@ require("./DB/index");
 //middle ware functions
 app.use(express.static("public"));
 app.use(express.json());
-app.use(express.static("public"))
+app.use(express.static("public"));
 // app.set('view engine','ejs');
 // app.set("view engine", "ejs");
 
@@ -28,16 +29,17 @@ app.use(express.static("public"))
 app.use("/api/v1/cart", cartRouter);
 app.use("/api/v1/category", categoriesRouter);
 app.use("/api/v1/product", productRouter);
-app.use("/api/v1/orders",ordersRouter);
-app.use("/api/v1/",userRouter);
+app.use("/api/v1/orders", ordersRouter);
+app.use("/api/v1/", userRouter);
 app.use("/api/v1", userRouter);
 // app.use(adminAuth)
-app.use("/api/v1/admin",adminAuth,adminRouter);
+app.use("/api/v1/admin", adminAuth, adminRouter);
 // app.use(userAuth)
-app.use("/api/v1/cart", userAuth,cartRouter);
-app.use("/api/v1/category", userAuth,categoriesRouter);
+app.use("/api/v1/cart", userAuth, cartRouter);
+app.use("/api/v1/category", userAuth, categoriesRouter);
 app.use("/api/v1/product", userAuth, productRouter);
 app.use("/api/v1/orders", userAuth, ordersRouter);
+app.use("/api/v1/payment", paymentRouter);
 // mongoose
 //   .connect("mongodb://localhost:27017/ECommerce")
 //   .then(() => {
@@ -47,4 +49,8 @@ app.use("/api/v1/orders", userAuth, ordersRouter);
 //     console.log(`Error In Connection: ${Error}`);
 //   });
 
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("http://localhost:" + PORT);
+});
